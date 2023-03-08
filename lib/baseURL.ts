5187367d9a9f86e0f713c4b71bd7ea78b85c8cb5
Ye Thiha <mightycryptowall@gmail.com>
2023-03-08 23:18:01 +0630

    [yethiha]: mocks data added

--- lib/baseURL.ts
+++ lib/baseURL.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const backendURL = "http://localhost:1337/api";
-
+export const backendURL = "http://192.168.99.9:1337/api";
 export const baseURL = axios.create({
     baseURL: backendURL,
 });
+
+export default baseURL;
+
--- tests/useGetAllPostQuery.test.ts
+++ tests/useGetAllPostQuery.test.ts
@@ -1,20 +1,32 @@
+import { baseURL } from './../lib/baseURL';
+import mockAxios from 'jest-mock-axios';
 import { rest } from 'msw'
 import * as React from 'react'
 import { renderHook, waitFor } from '@testing-library/react'
 // import { server } from '../setupTests'
 import { createWrapper } from './utils'
 import useGetAllPostQuery from '../queries/useGetAllPostQuery'
+import axios from "axios";
+import { get_all_posts } from '../apis/posts';
+import { getAllPostSuccess } from '../mocks/posts';
+jest.mock("./../lib/baseURL");
 
 describe('query hook', () => {
+    afterEach(() => {
+        mockAxios.reset();
+      });
     test('successful get all post query hook', async () => {
+        
+        
+    
+        (baseURL.get as jest.Mock).mockResolvedValueOnce(getAllPostSuccess);
         const { result } = renderHook(() => useGetAllPostQuery(), {
             wrapper: createWrapper()
         })
-
-        console.log(result.current);
+        
         await waitFor(() => expect(result.current.isSuccess).toBe(true))
 
         expect(result?.current?.data ? result?.current?.data[result?.current?.data.length -1].id : 0).toBe(1)
-        expect(result?.current?.data ? result?.current?.data[result?.current?.data.length -1].name : "").toBe("Banana City")
+        expect(result?.current?.data ? result?.current?.data[result?.current?.data.length -1].name : "").toBe("Banana Man")
     })
 })
