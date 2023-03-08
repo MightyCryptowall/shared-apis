037d0217857bc58d1b05b3ab9786b576319a0ff8
nayyaung9 <naygit.developer@gmail.com>
2023-03-08 17:01:56 +0630

    testing query invalidate

--- mutations/usePostMutation.ts
+++ mutations/usePostMutation.ts
@@ -1,11 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { add_new_post } from "../apis/posts";
 
 const usePostMutation = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: add_new_post,
     onError: (error: any) => {
       console.log({ error });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["get-all-posts"]);
     },
   });
 };
